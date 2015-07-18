e1d1a79dd97bacf2c5c4a43fbb11a78ce723d0ae
lionzxy <nikita@kulikof.ru>
2015-07-18 21:15:52 +0300

    add extends CommonProxy

--- src/main/java/com/lionzxy/mysticalislands/proxy/ClientProxy.java
+++ src/main/java/com/lionzxy/mysticalislands/proxy/ClientProxy.java
@@ -3,7 +3,7 @@ package com.lionzxy.mysticalislands.proxy;
 /**
  * Created by lionzxy on 18.07.15.
  */
-public class ClientProxy {
+public class ClientProxy extends CommonProxy{
 
     public void registerProxies(){
 
